de92ab6cd4d60efaf224d28b164dd5fead49541e
Harm Kolvoort <hkolvoort@gmail.com>
2024-11-15 22:37:10 +0100

    Added TestPropertySource annotation with H2 driver to ensure proper working of test without providing a profile.

--- src/test/java/nl/hkstwk/calculationmodule/CalculationModuleApplicationTests.java
+++ src/test/java/nl/hkstwk/calculationmodule/CalculationModuleApplicationTests.java
@@ -2,8 +2,10 @@ package nl.hkstwk.calculationmodule;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.datasource.driverClassName=org.h2.Driver")
 class CalculationModuleApplicationTests {
 
     @Test
