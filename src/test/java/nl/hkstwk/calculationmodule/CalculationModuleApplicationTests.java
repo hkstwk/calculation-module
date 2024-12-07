6421ee47bece6de55dc1efc43b8a611215029013
Harm Kolvoort <hkolvoort@gmail.com>
2024-12-07 23:41:18 +0100

    kubernetes yaml files updated; fixed broken test

--- src/test/java/nl/hkstwk/calculationmodule/CalculationModuleApplicationTests.java
+++ src/test/java/nl/hkstwk/calculationmodule/CalculationModuleApplicationTests.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ActiveProfiles("dev")
 @SpringBootTest
 class CalculationModuleApplicationTests {
 
